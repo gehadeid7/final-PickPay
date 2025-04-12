0627b15248b174c50ae9cf8d5d8b4dbfe56f7c1f
gehadeid7 <gehhadd7@gmail.com>
2025-04-12 20:01:57 +0200

    gg

---
+++ lib/core/services/firebase_auth_service.dart
@@ -0,0 +1,23 @@
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+
+// class FirebaseAuthService 
+// {
+//   Future<User> createUserWithEmailAndPassword({required String email, required String Password}) async
+//   {
+// try {
+//   final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+//     email: email,
+//     password: Password,
+//   );
+// } on FirebaseAuthException catch (e) {
+//   if (e.code == 'weak-password') {
+//     print('The password provided is too weak.');
+//   } else if (e.code == 'email-already-in-use') {
+//     print('The account already exists for that email.');
+//   }
+// } catch (e) {
+//   print(e);
+// }
+//   }
+// }
