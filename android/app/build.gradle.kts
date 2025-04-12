977aac84d093785d05051ae73959928753fb2d3b
kero ehab <fireherokoko@gmail.com>
2025-04-12 20:36:55 +0200

    minsdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,9 @@ android {
         applicationId = "com.example.pickpay"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 27
+        // minSdk = flutter.minSdkVersion
+
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
