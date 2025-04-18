a222faa1934672d79c78813fb5672967e09bb8b1
gehadeid7 <gehhadd7@gmail.com>
2025-04-18 13:07:07 +0200

    j

--- lib/core/widgets/home_item.dart
+++ lib/core/widgets/home_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pickpay/core/utils/app_text_styles.dart';
 
 class HomeItem extends StatelessWidget {
   const HomeItem({super.key});
@@ -7,7 +8,7 @@ class HomeItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 180,
-      height: 200,
+      height: 250,
       decoration: ShapeDecoration(
         color: Color(0xFFF3F5F7),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
@@ -17,7 +18,15 @@ class HomeItem extends StatelessWidget {
           Positioned.fill(
             child: Column(
               children: [
+                SizedBox(
+                  height :20
+                ),
                 Image.asset('assets/elecCat/AirPodsPro.jpeg'),
+                ListTile(
+                  title: Text('elec name',
+                  style: TextStyles.semiBold13
+                  ),
+                ),
               ],
             ),
           )
