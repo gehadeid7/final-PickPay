e1157e04116c986382b6a87fa3c72e02d6bda2ff
gehadeid7 <gehhadd7@gmail.com>
2025-04-19 02:44:27 +0200

    n

--- lib/features/home/presentation/views/widgets/bottom_navigation/active_item.dart
+++ lib/features/home/presentation/views/widgets/bottom_navigation/active_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pickpay/core/utils/app_colors.dart';
 import 'package:pickpay/core/utils/app_text_styles.dart';
 
 class ActiveItem extends StatelessWidget {
