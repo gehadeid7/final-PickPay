bbc4287ad8fb42f68231f31bae832e6a0e88d386
gehadeid7 <gehhadd7@gmail.com>
2025-05-05 10:38:48 +0300

    j

---
+++ lib/features/categories_pages/products_views/fashion_products_views/fashion_product3.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:pickpay/features/categories_pages/models/product_model.dart';
+import 'package:pickpay/features/categories_pages/products_views/product_detail_view.dart';
+
+class FashionProduct extends StatelessWidget {
+  const FashionProduct({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final product = ProductsViewsModel(
+      id: '',
+      title: '',
+      imagePaths: [
+        '',
+      ],
+      category: 'Fashion',
+      price: 9399.00,
+      originalPrice: 0.0,
+      rating: 5.0,
+      reviewCount: 88,
+      deliveryDate: 'Sunday, 9 March',
+      deliveryTimeLeft: '20hrs 36 mins',
+      deliveryLocation: 'Egypt',
+      inStock: true,
+      shipsFrom: 'Pickpay Warehouse',
+      soldBy: 'Pickpay Official',
+    );
+
+    return ProductDetailView(product: product);
+  }
+}
