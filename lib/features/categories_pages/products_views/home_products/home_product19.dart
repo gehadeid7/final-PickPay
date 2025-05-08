3913199be0574c79d2ee748d6c93d874956ea6e4
gehadeid7 <gehhadd7@gmail.com>
2025-05-08 07:34:56 +0300

    home files

---
+++ lib/features/categories_pages/products_views/home_products/home_product19.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:pickpay/features/categories_pages/models/product_model.dart';
+import 'package:pickpay/features/categories_pages/products_views/product_detail_view.dart';
+
+class HomeProduct19 extends StatelessWidget {
+  const HomeProduct19({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final product = ProductsViewsModel(
+      id: '',
+      title: "",
+      imagePaths: [],
+      price: 850,
+      originalPrice: 900,
+      rating: 3.1,
+      reviewCount: 81228,
+      deliveryDate: 'Sunday, 9 March',
+      deliveryTimeLeft: '20hrs 36 mins',
+      deliveryLocation: 'Egypt',
+      inStock: true,
+      shipsFrom: 'Pickpay',
+      soldBy: 'Pickpay',
+    );
+
+    return ProductDetailView(product: product);
+  }
+}
