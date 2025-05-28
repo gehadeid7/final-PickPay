4fb5fbd84b12c211a6d50753dcb8138e09db2bad
gehadeid7 <gehhadd7@gmail.com>
2025-05-28 15:42:47 +0300

    categories view

--- lib/features/home/presentation/views/widgets/categories_view_body.dart
+++ lib/features/home/presentation/views/widgets/categories_view_body.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pickpay/constants.dart';
 import 'package:pickpay/core/themes/theme_provider.dart';
-import 'package:pickpay/core/utils/app_text_styles.dart';
 import 'package:pickpay/features/home/presentation/cubits/categories_cubits/categories_cubits_cubit.dart';
 import 'package:pickpay/features/home/presentation/cubits/categories_cubits/categories_cubits_state.dart';
 import 'package:pickpay/features/home/presentation/views/widgets/category_navigation_helper.dart';
@@ -13,130 +12,160 @@ import 'package:pickpay/features/sub_categories/widgets/animated_subcategory_car
 class CategoriesViewBody extends StatelessWidget {
   const CategoriesViewBody({super.key});
 
-  List<Map<String, dynamic>> _getAllSubcategories(List<dynamic> categories) {
-    List<Map<String, dynamic>> allSubcategories = [];
+  Map<String, List<Map<String, dynamic>>> _getAllSubcategories(
+      List<dynamic> categories) {
+    final Map<String, List<Map<String, dynamic>>> categoryGroups = {
+      'Home': [
+        {
+          'name': 'Furniture',
+          'image': 'assets/Home_products/furniture/furniture4/1.png',
+          'index': 5,
+        },
+        {
+          'name': 'Home Decor',
+          'image': 'assets/Home_products/home-decor/home_decor4/1.png',
+          'index': 6,
+        },
+        {
+          'name': 'Bath & Bedding',
+          'image': 'assets/Home_products/bath_and_bedding/bath5/3.png',
+          'index': 7,
+        },
+        {
+          'name': 'Kitchen & Dining',
+          'image': 'assets/Home_products/kitchen/kitchen3/1.png',
+          'index': 8,
+        },
+      ],
+      'Fashion': [
+        {
+          'name': "Women's Fashion",
+          'image': 'assets/Fashion_products/Women_Fashion/women_fashion4/1.png',
+          'index': 9,
+        },
+        {
+          'name': "Men's Fashion",
+          'image': 'assets/Fashion_products/Men_Fashion/men_fashion4/1.png',
+          'index': 10,
+        },
+        {
+          'name': "Kids' Fashion",
+          'image': 'assets/Fashion_products/Kids_Fashion/kids_fashion4/2.png',
+          'index': 11,
+        },
+      ],
+      'Electronics': [
+        {
+          'name': 'Mobile & Tablets',
+          'image':
+              'assets/electronics_products/mobile_and_tablet/mobile_and_tablet1/1.png',
+          'index': 0,
+        },
+        {
+          'name': 'TVs',
+          'image': 'assets/electronics_products/tvscreens/tv2/2.png',
+          'index': 1,
+        },
+        {
+          'name': 'Laptop',
+          'image': 'assets/electronics_products/Laptop/Laptop2/1.png',
+          'index': 2,
+        },
+      ],
+      'Appliances': [
+        {
+          'name': 'Large Appliances',
+          'image': 'assets/appliances/product3/2.png',
+          'index': 3,
+        },
+        {
+          'name': 'Small Appliances',
+          'image': 'assets/appliances/product8/3.png',
+          'index': 4,
+        },
+      ],
+      'Beauty': [
+        {
+          'name': 'Makeup',
+          'image': 'assets/beauty_products/makeup_5/1.png',
+          'index': 12,
+        },
+        {
+          'name': 'Skincare',
+          'image': 'assets/beauty_products/skincare_3/1.png',
+          'index': 13,
+        },
+        {
+          'name': 'Haircare',
+          'image': 'assets/beauty_products/haircare_4/1.png',
+          'index': 14,
+        },
+        {
+          'name': 'Fragrance',
+          'image': 'assets/beauty_products/fragrance_2/1.png',
+          'index': 15,
+        },
+      ],
+      'Video Games': [
+        {
+          'name': 'Console',
+          'image': 'assets/subcategories/video_games/1.png',
+          'index': 16,
+        },
+        {
+          'name': 'Controllers',
+          'image': 'assets/subcategories/video_games/2.png',
+          'index': 17,
+        },
+        {
+          'name': 'Accessories',
+          'image': 'assets/subcategories/video_games/3.png',
+          'index': 18,
+        },
+      ],
+    };
 
-    // Electronics
-    allSubcategories.addAll([
-      {
-        'name': 'Mobile & Tablets',
-        'image':
-            'assets/electronics_products/mobile_and_tablet/mobile_and_tablet1/1.png',
-        'index': 0,
-      },
-      {
-        'name': 'TVs',
-        'image': 'assets/electronics_products/tvscreens/tv2/2.png',
-        'index': 1,
-      },
-      {
-        'name': 'Laptop',
-        'image': 'assets/electronics_products/Laptop/Laptop2/1.png',
-        'index': 2,
-      },
-    ]);
+    return categoryGroups;
+  }
 
-    // Appliances
-    allSubcategories.addAll([
-      {
-        'name': 'Large Appliances',
-        'image': 'assets/appliances/product3/2.png',
-        'index': 3,
-      },
-      {
-        'name': 'Small Appliances',
-        'image': 'assets/appliances/product8/3.png',
-        'index': 4,
-      },
-    ]);
-
-    // Home
-    allSubcategories.addAll([
-      {
-        'name': 'Furniture',
-        'image': 'assets/Home_products/furniture/furniture4/1.png',
-        'index': 5,
-      },
-      {
-        'name': 'Home Decor',
-        'image': 'assets/Home_products/home-decor/home_decor4/1.png',
-        'index': 6,
-      },
-      {
-        'name': 'Bath & Bedding',
-        'image': 'assets/Home_products/bath_and_bedding/bath5/3.png',
-        'index': 7,
-      },
-      {
-        'name': 'Kitchen & Dining',
-        'image': 'assets/Home_products/kitchen/kitchen3/1.png',
-        'index': 8,
-      },
-    ]);
-
-    // Fashion
-    allSubcategories.addAll([
-      {
-        'name': "Women's Fashion",
-        'image': 'assets/Fashion_products/Women_Fashion/women_fashion4/1.png',
-        'index': 9,
-      },
-      {
-        'name': "Men's Fashion",
-        'image': 'assets/Fashion_products/Men_Fashion/men_fashion4/1.png',
-        'index': 10,
-      },
-      {
-        'name': "Kids' Fashion",
-        'image': 'assets/Fashion_products/Kids_Fashion/kids_fashion4/2.png',
-        'index': 11,
-      },
-    ]);
-
-    // Beauty
-    allSubcategories.addAll([
-      {
-        'name': 'Makeup',
-        'image': 'assets/beauty_products/makeup_5/1.png',
-        'index': 12,
-      },
-      {
-        'name': 'Skincare',
-        'image': 'assets/beauty_products/skincare_3/1.png',
-        'index': 13,
-      },
-      {
-        'name': 'Haircare',
-        'image': 'assets/beauty_products/haircare_4/1.png',
-        'index': 14,
-      },
-      {
-        'name': 'Fragrance',
-        'image': 'assets/beauty_products/fragrance_2/1.png',
-        'index': 15,
-      },
-    ]);
-
-    // Video Games
-    allSubcategories.addAll([
-      {
-        'name': 'Console',
-        'image': 'assets/subcategories/video_games/1.png',
-        'index': 16,
-      },
-      {
-        'name': 'Controllers',
-        'image': 'assets/subcategories/video_games/2.png',
-        'index': 17,
-      },
-      {
-        'name': 'Accessories',
-        'image': 'assets/subcategories/video_games/3.png',
-        'index': 18,
-      },
-    ]);
-
-    return allSubcategories;
+  Widget _buildCategorySection(
+      String title, List<Map<String, dynamic>> items, bool isDarkMode) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: isDarkMode ? Colors.white : Colors.grey.shade800,
+            ),
+          ),
+        ),
+        GridView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 0.85,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+          ),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final subcategory = items[index];
+            return AnimatedSubcategoryCard(
+              name: subcategory['name'],
+              imagePath: subcategory['image'],
+              index: subcategory['index'],
+              onTap: () => navigateToCategory(context, subcategory['name']),
+            );
+          },
+        ),
+      ],
+    );
   }
 
   @override
@@ -147,13 +176,13 @@ class CategoriesViewBody extends StatelessWidget {
     return SingleChildScrollView(
       physics: const BouncingScrollPhysics(),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(height: kTopPadding),
           const Padding(
             padding: EdgeInsets.symmetric(horizontal: 16),
             child: CustomHomeAppbar(),
           ),
-          const SizedBox(height: 22),
           BlocBuilder<CategoriesCubit, CategoriesState>(
             builder: (context, state) {
               if (state is CategoriesLoading) {
@@ -161,31 +190,19 @@ class CategoriesViewBody extends StatelessWidget {
                   child: CircularProgressIndicator.adaptive(),
                 );
               } else if (state is CategoriesLoaded) {
-                final allSubcategories = _getAllSubcategories(state.categories);
+                final categoryGroups = _getAllSubcategories(state.categories);
 
                 return Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: GridView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 0.85,
-                      crossAxisSpacing: 16,
-                      mainAxisSpacing: 16,
-                    ),
-                    itemCount: allSubcategories.length,
-                    itemBuilder: (context, index) {
-                      final subcategory = allSubcategories[index];
-                      return AnimatedSubcategoryCard(
-                        name: subcategory['name'],
-                        imagePath: subcategory['image'],
-                        index: subcategory['index'],
-                        onTap: () =>
-                            navigateToCategory(context, subcategory['name']),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: categoryGroups.entries.map((entry) {
+                      return _buildCategorySection(
+                        entry.key,
+                        entry.value,
+                        isDarkMode,
                       );
-                    },
+                    }).toList(),
                   ),
                 );
               } else if (state is CategoriesError) {
--- lib/features/sub_categories/widgets/animated_subcategory_card.dart
+++ lib/features/sub_categories/widgets/animated_subcategory_card.dart
@@ -24,23 +24,156 @@ class _AnimatedSubcategoryCardState extends State<AnimatedSubcategoryCard>
   late AnimationController _controller;
   late Animation<Alignment> _alignmentAnimation;
   bool _isHovered = false;
-
   static const List<List<Color>> _gradientColorsLight = [
-    [Color(0xFFA8DADC), Color(0xFF457B9D)], // Soft cyan to muted blue
-    [Color(0xFFF1FAEE), Color(0xFFA8DADC)], // Very light mint to soft cyan
-    [Color(0xFFEAEAEA), Color(0xFFB0BEC5)], // Light grey to cool steel blue
-    [Color(0xFFD4ECDD), Color(0xFF8FB9A8)], // Pale green to sage green
-    [Color(0xFFF9F7F7), Color(0xFFD3C0CD)], // Soft off-white to muted mauve
-    [Color(0xFFF4E1D2), Color(0xFFBC9CA3)], // Light peach to dusty rose
+    [
+      Color(0xFF9EC1D9),
+      Color(0xFF7A9BCB)
+    ], // Soft sky blue to muted cornflower blue (mobile)
+    [
+      Color(0xFFB3CAED),
+      Color(0xFF8DB4F3)
+    ], // Light powder blue to pastel blue (tvs)
+    [
+      Color(0xFF9EC1D9),
+      Color(0xFF7A9BCB)
+    ], // Soft sky blue to muted cornflower blue (laptop)
+    [
+      Color(0xFF90B6B6),
+      Color(0xFF78A5A5)
+    ], // Pale teal to seafoam green (large appliances)
+    [
+      Color(0xFF90B6B6),
+      Color(0xFF78A5A5)
+    ], // Pale teal to seafoam green (small appliances)
+    [
+      Color(0xFFBDBDBD),
+      Color(0xFFA0A0A0)
+    ], // Light gray to medium gray (furniture)
+    [
+      Color(0xFF7A8B8E),
+      Color(0xFF5B6B6E)
+    ], // Slate gray to steel blue (home decor)
+    [
+      Color(0xFF6E7582),
+      Color(0xFF55606C)
+    ], // Light indigo-gray to charcoal steel (bath)
+    [
+      Color(0xFF6B6B6B),
+      Color(0xFF4E4E4E)
+    ], // Medium gray to dark slate gray (kitchen)
+
+    [
+      Color(0xFFD7B89E),
+      Color(0xFFB88C72)
+    ], // Warm caramel to light mocha (women's fashion)
+    [
+      Color(0xFFB58873),
+      Color(0xFF9A6E57)
+    ], // Soft brown to chocolate tan (men's fashion)
+    [
+      Color(0xFFA47B65),
+      Color(0xFF86624B)
+    ], // Light espresso to warm cocoa (kids' fashion)
+
+    [
+      Color(0xFFFFF0F3),
+      Color(0xFFF5C6D1)
+    ], // Very light pink to soft blush (makeup)
+    [
+      Color(0xFFFFF5F9),
+      Color(0xFFF5C6D1)
+    ], // Pale pink mist to gentle blush (skincare)
+    [
+      Color(0xFFF9C5D3),
+      Color(0xFFF59BB3)
+    ], // Soft blush to warm pink (haircare)
+    [
+      Color(0xFFFADFE1),
+      Color(0xFFF7C5C9)
+    ], // Pale pink to cotton rose (fragrance)
+
+    [
+      Color(0xFF8C6FCA),
+      Color(0xFFA78AD7)
+    ], // Light eggplant purple to soft violet (consoles)
+    [
+      Color(0xFFA183D0),
+      Color(0xFFC38FDB)
+    ], // Medium violet to gentle purple (controllers)
+    [
+      Color(0xFF9B7BCF),
+      Color(0xFFBEA2DD)
+    ], // Soft strong purple to light violet (accessories)
   ];
 
   static const List<List<Color>> _gradientColorsDark = [
-    [Color(0xFF1D3557), Color(0xFF457B9D)], // Deep navy to muted blue
-    [Color(0xFF2A3A40), Color(0xFF5C6B73)], // Dark slate to dusty grey-blue
-    [Color(0xFF36454F), Color(0xFF576F72)], // Charcoal to teal-grey
-    [Color(0xFF3B3A40), Color(0xFF6D7B8D)], // Dark charcoal to muted blue-grey
-    [Color(0xFF5C4D7D), Color(0xFF7E6A9E)], // Dusty purple to lavender-grey
-    [Color(0xFF4B474A), Color(0xFF7E7A82)], // Dark grey to soft lavender-grey
+    [Color(0xFF254E7A), Color(0xFF3A639A)], // Royal navy to soft denim (mobile)
+    [Color(0xFF2E5D9F), Color(0xFF4B7ACD)], // Medium blue to pastel blue (tvs)
+    [Color(0xFF254E7A), Color(0xFF3A639A)], // Royal navy to soft denim (laptop)
+    [
+      Color(0xFF2E4F4F),
+      Color(0xFF416D6D)
+    ], // Deep teal to soft sea green (large appliances)
+    [
+      Color(0xFF3B6060),
+      Color(0xFF557E7E)
+    ], // Dark slate teal to muted turquoise (small appliances)
+    [
+      Color(0xFF424242),
+      Color(0xFF757575)
+    ], // Cool dark grey to medium grey (furniture)
+    [
+      Color(0xFF263238),
+      Color(0xFF455A64)
+    ], // Blue-grey dark to steel (original reused) (home decor)
+    [
+      Color(0xFF1C1C2D),
+      Color(0xFF36454F)
+    ], // Indigo-graphite to charcoal steel (bath)
+    [
+      Color(0xFF121212),
+      Color(0xFF3D3D3D)
+    ], // Pitch black to slate grey (kitchen)
+
+    [
+      Color(0xFF6D4C41),
+      Color(0xFF8D6E63)
+    ], // Cocoa brown to soft mocha (women's fashion)
+    [
+      Color(0xFF5D4037),
+      Color(0xFF795548)
+    ], // Dark chocolate to medium brown (men's fashion)
+    [
+      Color(0xFF4E342E),
+      Color(0xFF6D4C41)
+    ], // Espresso brown to cocoa (kids'fashion)
+
+    [
+      Color(0xFFFFEBEE),
+      Color(0xFFF8BBD0)
+    ], // Very light pink to soft blush (makeup)
+    [
+      Color(0xFFFCE4EC),
+      Color(0xFFF8BBD0)
+    ], // Lightest pink mist to gentle blush  (skincare)
+    [
+      Color(0xFFF8BBD0),
+      Color(0xFFF48FB1)
+    ], // Soft blush to warm pink (haircare)
+    [
+      Color(0xFFFADADD),
+      Color(0xFFF5C2C7)
+    ], // Pale pink to cotton rose (fragrance)
+
+    [
+      Color(0xFF4527A0),
+      Color(0xFF7E57C2)
+    ], // Eggplant purple to soft violet (consoles)
+    [Color(0xFF5E35B1), Color(0xFF9C27B0)], // Violet blend (controllers)
+    [
+      Color(0xFF4A148C),
+      Color(0xFF9575CD)
+    ], // Strong purple to light violet (accesories)
   ];
 
   @override
@@ -107,14 +240,11 @@ class _AnimatedSubcategoryCardState extends State<AnimatedSubcategoryCard>
                 child: Stack(
                   fit: StackFit.expand,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Hero(
-                        tag: widget.imagePath,
-                        child: Image.asset(
-                          widget.imagePath,
-                          fit: BoxFit.contain,
-                        ),
+                    Hero(
+                      tag: widget.imagePath,
+                      child: Image.asset(
+                        widget.imagePath,
+                        fit: BoxFit.contain,
                       ),
                     ),
                     // Subtle gradient overlay for depth
@@ -152,8 +282,12 @@ class _AnimatedSubcategoryCardState extends State<AnimatedSubcategoryCard>
                       bottom: 0,
                       child: Container(
                         padding: const EdgeInsets.symmetric(
-                          vertical: 12,
-                          horizontal: 12,
+                          vertical: 8,
+                          horizontal: 8,
+                        ),
+                        constraints: const BoxConstraints(
+                          minHeight:
+                              36, // Set a minimum height that fits one line well
                         ),
                         decoration: BoxDecoration(
                           gradient: LinearGradient(
