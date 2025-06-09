f060fbd212d2f3dbf0a029ec50a405ef008aa999
gehadeid7 <gehhadd7@gmail.com>
2025-06-09 21:57:50 +0300

    color option

--- lib/features/related_products_widget/related_products.dart
+++ lib/features/related_products_widget/related_products.dart
@@ -118,7 +118,76 @@ class RelatedProducts extends StatelessWidget {
       return (widget.build(context) as ProductDetailView).product;
     }
     if (widget is FashionProduct2) {
-      return (widget.build(context) as ProductDetailView).product;
+      final colorImages = {
+        'Power Pink': [
+          'assets/Fashion_products/Women_Fashion/women_fashion2/1.png',
+          'assets/Fashion_products/Women_Fashion/women_fashion2/2.png',
+          'assets/Fashion_products/Women_Fashion/women_fashion2/3.png',
+        ],
+        'Sky Tint': [
+          'assets/Fashion_products/Women_Fashion/women_fashion2/sky_tint/1.png',
+          'assets/Fashion_products/Women_Fashion/women_fashion2/sky_tint/2.png',
+        ],
+        'Core Black': [
+          'assets/Fashion_products/Women_Fashion/women_fashion2/core_black/1.png',
+        ],
+      };
+
+      return ProductsViewsModel(
+        id: '682b00c26977bd89257c0e8f',
+        title: 'adidas womens ULTIMASHOW Shoes',
+        imagePaths: colorImages['Power Pink'],
+        colorImages: colorImages,
+        price: 2600,
+        originalPrice: 2750,
+        rating: 4.4,
+        reviewCount: 252,
+        category: 'Fashion',
+        subcategory: "Women's Fashion",
+        colorOptions: [
+          'Power Pink',
+          'Cloud White',
+          'Sky Tint',
+          'Core Black',
+        ],
+        colorAvailability: {
+          'Power Pink': true,
+          'Cloud White': true,
+          'Sky Tint': true,
+          'Core Black': false,
+        },
+        availableSizes: [
+          '37',
+          '38',
+          '39',
+          '40',
+          '41',
+          '42',
+        ],
+        sizeAvailability: {
+          '37': false,
+          '38': true,
+          '39': false,
+          '40': true,
+          '41': false,
+          '42': true,
+        },
+        careInstruction: 'Mesh',
+        soleMaterial: 'Synthetic',
+        outerMaterial: 'Mesh',
+        innerMaterial: 'Manmade',
+        aboutThisItem: '''Jogging shoes
+Lace closure
+Water Type : Hot And Cold
+Textile upper
+REGULAR FIT''',
+        deliveryDate: 'Sunday, 9 March',
+        deliveryTimeLeft: '20hrs 36 mins',
+        deliveryLocation: 'Egypt',
+        inStock: true,
+        shipsFrom: 'Pickpay Warehouse',
+        soldBy: 'Pickpay Official',
+      );
     }
     if (widget is FashionProduct3) {
       return (widget.build(context) as ProductDetailView).product;
