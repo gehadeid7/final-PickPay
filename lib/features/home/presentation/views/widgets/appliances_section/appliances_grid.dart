18e4bc99d77d2b93e30014ebc6b4dc7e5d9dc2fd
gehadeid7 <gehhadd7@gmail.com>
2025-06-23 16:25:25 +0300

    appliances bestsellers grid

--- lib/features/home/presentation/views/widgets/appliances_section/appliances_grid.dart
+++ lib/features/home/presentation/views/widgets/appliances_section/appliances_grid.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:pickpay/features/categories_pages/products_views/appliances_products_views/appliances_product1.dart';
+import 'package:pickpay/features/categories_pages/products_views/appliances_products_views/appliances_product10.dart';
+import 'package:pickpay/features/categories_pages/products_views/appliances_products_views/appliances_product11.dart';
+import 'package:pickpay/features/categories_pages/products_views/appliances_products_views/appliances_product12.dart';
+import 'package:pickpay/features/categories_pages/products_views/appliances_products_views/appliances_product14.dart';
 import 'package:pickpay/features/categories_pages/products_views/appliances_products_views/appliances_product15.dart';
+import 'package:pickpay/features/categories_pages/products_views/appliances_products_views/appliances_product4.dart';
 import 'package:pickpay/features/categories_pages/products_views/appliances_products_views/appliances_product6.dart';
 import 'package:pickpay/features/categories_pages/products_views/appliances_products_views/appliances_product7.dart';
 import 'package:pickpay/features/home/presentation/views/widgets/card_item.dart';
@@ -18,13 +23,48 @@ class _AppliancesGridState extends State<AppliancesGrid> {
 
   final List<Map<String, dynamic>> items = [
     {
-      'imagePath': 'assets/appliances/product6/1.png',
+      'imagePath': 'assets/appliances/product15/1.png',
       'productName':
-          'deime Air Fryer 6.2 Quart, Large Air Fryer for Families, 5 Cooking Functions AirFryer, 400°F Temp Controls in 5° Increments, Ceramic Coated Nonstick',
-      'price': '3629',
+          ' BLACK & DECKER Dough Mixer With 1000W 3-Blade Motor And 4L Stainless Steel Mixer For 600G Dough Mixer 5.76 Kilograms White/Sliver',
+      'price': '6799',
+      'rating': 4.6,
+      'reviewCount': 1735,
+      'detailPage': const AppliancesProduct15(),
+    },
+    {
+      'imagePath': 'assets/appliances/product14/1.png',
+      'productName':
+          'Black & Decker 1.7L Concealed Coil Stainless Steel Kettle, Jc450-B5, Silver',
+      'price': '1594',
       'rating': 4.5,
-      'reviewCount': 954,
-      'detailPage': const AppliancesProduct6(),
+      'reviewCount': 1162,
+      'detailPage': const AppliancesProduct14(),
+    },
+    {
+      'imagePath': 'assets/appliances/product12/1.png',
+      'productName':
+          'TORNADO Gas Water Heater 6 Liter, Digital, Natural Gas, Silver GHM-C06CNE-S',
+      'price': '3719',
+      'rating': 4.5,
+      'reviewCount': 12,
+      'detailPage': const AppliancesProduct12(),
+    },
+    {
+      'imagePath': 'assets/appliances/product11/1.png',
+      'productName':
+          'Fresh fan 50 watts 18 inches with charger with 3 blades, black and white',
+      'price': '3983',
+      'rating': 4.4,
+      'reviewCount': 674,
+      'detailPage': const AppliancesProduct11(),
+    },
+    {
+      'imagePath': 'assets/appliances/product10/1.png',
+      'productName': 'Fresh 1600W Faster Vacuum Cleaner with Bag, Black',
+      'price': '2830',
+      'rating': 4.6,
+      'reviewCount': 4576,
+      'detailPage': const AppliancesProduct10(),
     },
     {
       'imagePath': 'assets/appliances/product7/1.png',
@@ -35,22 +75,22 @@ class _AppliancesGridState extends State<AppliancesGrid> {
       'detailPage': const AppliancesProduct7(),
     },
     {
-      'imagePath': 'assets/appliances/product15/1.png',
+      'imagePath': 'assets/appliances/product6/1.png',
       'productName':
-          ' BLACK & DECKER Dough Mixer With 1000W 3-Blade Motor And 4L Stainless Steel Mixer For 600G Dough Mixer 5.76 Kilograms White/Sliver',
-      'price': '6799',
-      'rating': 4.6,
-      'reviewCount': 1735,
-      'detailPage': const AppliancesProduct15(),
+          'deime Air Fryer 6.2 Quart, Large Air Fryer for Families, 5 Cooking Functions AirFryer, 400°F Temp Controls in 5° Increments, Ceramic Coated Nonstick',
+      'price': '3629',
+      'rating': 4.5,
+      'reviewCount': 954,
+      'detailPage': const AppliancesProduct6(),
     },
     {
-      'imagePath': 'assets/appliances/product1/1.png',
+      'imagePath': 'assets/appliances/product4/1.png',
       'productName':
-          'Koldair Water Dispenser Cold And Hot 2 Tabs - Bottom Load KWDB Silver Cooler',
-      'price': '10499',
-      'rating': 4.9,
-      'reviewCount': 1893,
-      'detailPage': const AppliancesProduct1(),
+          'Zanussi Automatic Washing Machine, Silver, 8 KG - ZWF8240SX5r',
+      'price': '17023',
+      'rating': 4.2,
+      'reviewCount': 14,
+      'detailPage': const AppliancesProduct4(),
     },
   ];
 
